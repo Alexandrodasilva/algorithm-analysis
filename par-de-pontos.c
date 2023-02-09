b17e255c051c67dedd92471234eae2a8853319c4
alexandrodasilva <alexsamdrosilva13@gmail.com>
2023-02-09 11:15:42 -0300

    adicionando inicio de  um problema pra resolver

---
+++ par-de-pontos.c
@@ -0,0 +1,30 @@
+
+// Par de pontos mais próximo
+
+// Descrição
+// Dado um conjunto de pontos em posição geral no plano, encontre a menor distância entre quaisquer dois pontos do conjunto.
+// Cada ponto é descrito por suas coordenadas no plano. Todas as coordenadas são números inteiros entre 0 e 999.
+
+// Formato de entrada
+// A entrada é composta por uma linha contendo o número n representando o número de pontos do conjunto, seguida por n linhas contendo dois números cada contendo as coordenadas de cada ponto.
+// EX:
+// 5
+// 136 729
+// 917 29
+// 547 533
+// 892 881
+// 247 29
+
+// Formato de saída
+// A saída é um número real com quatro casas decimais de precisão representando a menor distância entre qualquer par de pontos.
+// EX:
+// 455.3427
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+	return 0;
+}
