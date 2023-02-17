eac2d1b3f21f1f198a56a6f387d1c2f4063fd328
alexandro de jesus silva <alexsamdrosilva13@gmail.com>
2023-02-17 00:30:12 -0300

    terminando de implementar o algoritmo de intercalação

--- intercalacao.c
+++ intercalacao.c
@@ -16,9 +16,11 @@
 // entrada:
 // 3
 // 4
+
 // 2
 // 4
 // 8
+
 // 3
 // 5
 // 9
@@ -38,8 +40,26 @@
 #include <math.h>
 #include <stdlib.h>
 
+void intercalar(int *linha1, int *linha2, int quantL1, int quantL2){
+    int quant_total = quantL1 + quantL2;
+    int i, v3[quant_total], j = 0;
+    for(i = 0; i <= quant_total && j < quantL2; i+=2 ){
+        if(j < quantL1 && j < quantL2){
+            v3[i] = linha1[j];
+            v3[i+1] = linha2[j];
+            j++;  
+        }
+        else{
+            v3[i] = linha2[j];
+        }
+    }
+    for(i = 0; i < quant_total; i++){
+        printf("%d\n", v3[i]);
+    }
+}
+
 int main() {
-    int quantL1, quantL2, i = 1;
+    int quantL1, quantL2, i = 0, j = 0, quant_total;
 
     scanf("%d", &quantL1);
     scanf("%d", &quantL2);
@@ -47,18 +67,18 @@ int main() {
     int linha1[quantL1];
     int linha2[quantL2];
 
-    while (i <= quantL1)
+    while (i < quantL1)
     {
         scanf("%d", &linha1[i]);
         i++;
     }
-    while (i <= quantL2)
+
+    while (j < quantL2)
     {
-        scanf("%d", &linha1[i]);
-        i++;
+        scanf("%d", &linha2[j]);
+        j++;
     }
-    
-    
-    
+
+    intercalar(linha1, linha2, quantL1, quantL2);
 	return 0;
 }
