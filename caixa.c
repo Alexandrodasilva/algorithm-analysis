cb0e6fef93fdbfdfb2d129d5c5b548a5d5959389
alexandro de jesus silva <alexsamdrosilva13@gmail.com>
2023-04-11 15:35:19 -0300

    add

---
+++ caixa.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+typedef struct {
+	int x;
+    int x;
+} pontos;
+
+insertionsort, busca binaria; realoc
+
+int main() {
+    int i, x, y, grade, num_caixas;
+    scanf("%d", &num_caixas);
+    for(i = 1; i < num_caixas; i++){
+        scanf("%d");
+    }
+    
+	return 0;
+}
