dc046cf06f7dd2bba83de3d9c593b66ad9bc5ab4
alexandrodasilva <alexsamdrosilva13@gmail.com>
2023-07-02 10:26:56 -0300

    matriz que calcula qualquer nxn quadrada do traço da matriz

--- line-trace.c
+++ line-trace.c
@@ -5,23 +5,24 @@
 
 int main(){
     int n;
-    float traco[3][3], vt[3], soma = 0;
     scanf("%d", &n);
-
+    float traco[n][n],soma = 0;
+    
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
             scanf("%f", &traco[i][j]);
         }
     }
-
-    for(int i = 0; i < 3; i++){
-        vt[i] = traco[i][i];
+    for(int i = 0; i < n; i++){
+        soma += traco[i][i];
     }
-
-    for(int i = 0; i < 3; i++){
-        soma += vt[i];
+    printf("tr(A) = ");
+    for(int i = 0; i < n; i++){
+        printf("(%.2f) ", traco[i][i]);
+        if(i < n -1){
+            printf("+ ");
+        }
     }
-    
-    printf("tr(A) = (%.2f) + (%.2f) + (%.2f) = %.2f\n", vt[0], vt[1], vt[2], soma);
+    printf("= %.2f\n", soma);
     return 0;
 }
