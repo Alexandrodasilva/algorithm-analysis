7e6a6735a7e85cbab6d85d46098c7e264c76da43
alexandrodasilva <alexsamdrosilva13@gmail.com>
2023-07-16 15:45:03 -0300

    implementado a forma da saída do algoritmo. Adicionado o nome do algoritmo a ser implementado

--- challenge.c
+++ challenge.c
@@ -5,6 +5,7 @@
 
 void derivada(int *coeficiente, int *expoente){
     int tamanho = 0, i;
+
     while (coeficiente[tamanho] != -1){
         tamanho++;
     }
@@ -12,29 +13,37 @@ void derivada(int *coeficiente, int *expoente){
     for(i = 0; i < tamanho; i++){
         coeficiente[i] *= expoente[i];
         expoente[i] -= 1;
-         printf("coe=%d expo=%d\n", coeficiente[i], expoente[i]);
     }
 
 }
 int main() {
     int n, i;
-    scanf("%d", &n);
+    //scanf("%d", &n);
     int coeficiente[n], expoente[n];
     char incognita[n];
-    //while(scanf("%d", &n) != EOF){
-       // if(n == 0){
-         //   break;
-       // }
+    while(scanf("%d", &n) != EOF){
+        if(n == 0){
+           break;
+        }
         for(i = 0; i < n; i++){
             scanf("%d %c %d", &coeficiente[i], &incognita[i], &expoente[i]);
         }
         coeficiente[n] = -1;
         expoente[n] = -1;
-    //}
-    derivada(coeficiente, expoente);
-    // printf("f'(x)=")
-    for(int i = 0; i < n; i++){
-         printf("coe=%d expo=%d ", coeficiente[i], expoente[i]);
     }
+    derivada(coeficiente, expoente);
+    printf("f'(x)=+");
+    for(int i = 0; i < n; i++){
+        if(expoente[i] ==  0){
+            printf("%d", coeficiente[i]);
+        }
+        else{
+            printf("%d%c^%d", coeficiente[i], incognita[i], expoente[i]);
+            if(i < n-1 ){
+                printf("+");
+            }
+        }
+    }
+    printf("\n");
 	return 0;
 }
