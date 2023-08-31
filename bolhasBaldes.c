dc3924c22db35e05f2b90d5da64cc32f5ffda8a7
alexandrodasilva <alexsamdrosilva13@gmail.com>
2023-08-31 11:43:36 -0300

    comecei com atividade de fazer codigo bolhasBaldes, faltando fazer o algoritmo de mergesort para ordenar

---
+++ bolhasBaldes.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int megesort(int *seq, int n){
+    
+}
+
+int main() {
+    int n, seq[100000];
+    
+    while (1) {
+        scanf("%d", &n);
+        if (n == 0) break;
+        for (int i = 0; i < n; i++) {
+            scanf("%d", &seq[i]);
+        }
+
+       //utilizar o algoritmo de megesort
+        // int aux = 0;
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = i+1; j < n; j++) {
+        //         if (seq[i] > seq[j]) {
+        //             aux++;
+        //         }
+        //     }
+        // }
+
+        if ( megesort(seq, n)% 2 == 0) {
+            printf("Marcelo\n");
+        } else {
+            printf("Carlos\n");
+        }
+    }
+    
+    return 0;
+}
