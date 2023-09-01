3ea0d1780263b79996b9cdb84d4622c4f26f194a
alexandro de jesus silva <alexsamdrosilva13@gmail.com>
2023-09-01 06:36:24 -0300

    ajuste no mergesort

--- bolhasBaldes.c
+++ bolhasBaldes.c
@@ -3,45 +3,44 @@
 #include <math.h>
 #include <stdlib.h>
 
-int merge(int *seq, int low, int mid, int high, int aux){
-    int i,j,k,t[100000];
+
+void merge(int *seq, int low, int mid, int high, int *aux) {
+    int i, j, k;
+    int t[100000];
+
     i = low;
-    j = mid +1;
-    k=low;
-    while (i <= mid && j <= high)
-    {
-        if(seq[i] >= seq[j]){
-            t[k++] = seq[j++];
-            aux ++;
-        }else{
+    j = mid + 1;
+    k = low;
+
+    while (i <= mid && j <= high) {
+        (*aux)++;
+        if (seq[i] <= seq[j]) {
             t[k++] = seq[i++];
+        } else {
+            t[k++] = seq[j++];
         }
     }
-    while (i <=mid)
-    {
+
+    while (i <= mid) {
         t[k++] = seq[i++];
     }
-    while (j <= high)
-    {
+
+    while (j <= high) {
         t[k++] = seq[j++];
     }
-    for(i= low; i<=high; i++){
+
+    for (i = low; i <= high; i++) {
         seq[i] = t[i];
-    } 
-    return aux;
+    }
 }
 
-int megesort(int *seq, int low, int high, int aux){
-    //uso do algoritmo merge
-    int mid;
-    if(low != high){
-        mid= (low+high)/2;
-        megesort(seq, low, high, aux);
+void megesort(int *seq, int low, int high, int *aux){
+    if(low < high){
+        int mid = (low + high)/2;
+        megesort(seq, low, mid, aux);
         megesort(seq, mid+1, high, aux);
         merge(seq, low, mid, high, aux);
     }
-    return aux;
-
 }
 
 int main() {
@@ -57,7 +56,9 @@ int main() {
         low = 0;
         high = n - 1;
         aux = 0;
-        if( megesort(seq, low, high, aux)% 2 == 0) {
+        megesort(seq, low, high, &aux);
+        printf("(%d)\n", aux);
+        if( aux % 2 == 0) {
             printf("Marcelo\n");
         } else {
             printf("Carlos\n");
@@ -66,13 +67,3 @@ int main() {
     
     return 0;
 }
-
-       //utilizar o algoritmo de selectioSort
-        // int aux = 0;
-        // for (int i = 0; i < n; i++) {
-        //     for (int j = i+1; j < n; j++) {
-        //         if (seq[i] > seq[j]) {
-        //             aux++;
-        //         }
-        //     }
-        // }
